2419044f44651730b3836576d7d4030af598845d
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-03-11 19:12:04 -0500

    21.2 buggy as shite. imuo. server wont start

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts',
   {
-    useCreateIndex: true,
-    useNewUrlParser: true,
+    useNewUrlParser: true, 
     useUnifiedTopology: true,
   }
 );
